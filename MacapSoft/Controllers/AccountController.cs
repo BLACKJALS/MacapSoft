cb0c60081917e876fbbabe3d70f6d1e81c804c77
BLACKJALS <jholeon@uan.edu.co>
2017-04-09 18:23:41 -0500

    Se realiza el menú principal, vistas y esquema de página.

--- MacapSoft/Controllers/AccountController.cs
+++ MacapSoft/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace MacapSoft.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "HistoriaClinica");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
